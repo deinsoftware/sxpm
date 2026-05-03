1cb66aa07c07a47d336f9c9933437ce702b9ab54
equiman <equiman@users.noreply.github.com>
2026-05-02 20:05:50 -0500

    chore: remove unused dependencies (find-up, semver) and their re-exports

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,3 @@ export type { TranslateArgsParams, TranslateArgsResult } from './translator/tran
 export { translateCommand } from './translator/translate-command.js'
 export { translateArgs, cleanFlag } from './translator/translate-args.js'
 export { getPackageConfig, packageExists, availablePackages } from './managers/index.js'
-
-// Utilities needed by sxpm
-export { findUp } from 'find-up'
-export { default as semver } from 'semver'
